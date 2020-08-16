87705582bd13179201e98e103d0d9aee9ed80d42
Kai Quander <kq1003@gmail.com>
2020-08-15 21:01:17 -0400

    Components and start contact page

---
+++ my-app/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                Hello, this is a contact page
+            </div>
+        );
+    }
+}
+
+export default Contact;
